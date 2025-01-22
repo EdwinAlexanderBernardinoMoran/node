25a6c1dc946699adb3716cc8f8fda124b2f1426e
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-01-22 09:01:05 -0600

    Factory Functions - Aplicado

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,7 +1,8 @@
 // const { emailTemplate} = require('./js-foundation/01-template')
 // require('./js-foundation/02-destructuring')
 // const {getUserById} = require('./js-foundation/04-arrow-function');
-require('./js-foundation/05-factory');
+const { getAge, getUuid } = require('./plugins');
+const { buildMakePerson } = require('./js-foundation/05-factory');
 
 // console.log(emailTemplate);
 
@@ -11,3 +12,13 @@ require('./js-foundation/05-factory');
 //     if (error)  throw new Error(error);
 //     console.log('User found:', user);
 // });
+
+const makePerson = buildMakePerson({getUuid, getAge});
+
+const obj = {
+    name: "Alexander",
+    birthdate: "1994-09-02",
+}
+
+const john = makePerson(obj);
+console.log(john);
--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -1,21 +1,27 @@
 // const { v4: uuidv4 } = require('uuid');
 // const getAge = require('get-age')
-const { getUuid } = require('../plugins/get-uuid.plugin');
-const { getAge } = require('../plugins/get-age.plugin');
+// const { getUuid } = require('../plugins/get-uuid.plugin');
+// const { getAge } = require('../plugins/get-age.plugin');
 
-const obj = {
-    name: "Alexander",
-    birthdate: "1994-09-02",
-}
+// const obj = {
+//     name: "Alexander",
+//     birthdate: "1994-09-02",
+// }
 
-const buildPerson = ({name, birthdate}) => {
-    return {
-        id: getUuid(),
-        name: name,
-        birthdate: birthdate,
-        age: getAge(birthdate),
+const buildMakePerson = ({getUuid, getAge}) => {
+    return ({name, birthdate}) => {
+        return {
+            id: getUuid(),
+            name: name,
+            birthdate: birthdate,
+            age: getAge(birthdate),
+        }
     }
 }
 
-const jhon = buildPerson(obj);
-console.log(jhon);
+// const jhon = buildPerson(obj);
+// console.log(jhon);
+
+module.exports = {
+    buildMakePerson
+};
