b39d4a4d6192381f3c3f0a28f1829b40d989cd04
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-01-22 20:25:06 -0600

    Axios - Cliente para peticiones Http

--- 02-bases/src/plugins/http-client.plugin.js
+++ 02-bases/src/plugins/http-client.plugin.js
@@ -1,7 +1,11 @@
+const axios = require('axios');
+
 const httpClientPlugin = {
     get: async (url) => {
-        const response = await fetch(url);
-        return await response.json();
+        // const response = await fetch(url);
+        // return await response.json();
+        const { data } = await axios.get(url);
+        return data;
     },
 };
 
