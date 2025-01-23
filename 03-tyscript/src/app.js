01f1888095a2246005491e0d88e7687215dd31d8
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-01-23 11:29:49 -0600

    TyScript -Proyecto basico

---
+++ 03-tyscript/src/app.js
@@ -0,0 +1,22 @@
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+];
+
+const findHeroById = (id) => heroes.find(hero => hero.id === id);
+
+const hero = findHeroById(4);
+console.log(hero?.name ?? 'Hero not found'); // { id: 2, name: 'Spiderman', owner: 'Marvel' }
