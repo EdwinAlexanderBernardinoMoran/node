89d1e97d4e50d035bb6fca00cbbc6f09ec1503ac
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-01-23 13:40:09 -0600

    Creacion de scripts

--- 03-tyscript/src/app.ts
+++ 03-tyscript/src/app.ts
@@ -18,5 +18,5 @@ const heroes = [
 
 const findHeroById = (id: number) => heroes.find(hero => hero.id === id);
 
-const hero = findHeroById(4);
-console.log(hero?.name ?? 'Hero not found !!'); // { id: 2, name: 'Spiderman', owner: 'Marvel' }
+const hero = findHeroById(2);
+console.log(hero?.name ?? 'Hero not found!'); // { id: 2, name: 'Spiderman', owner: 'Marvel' }
